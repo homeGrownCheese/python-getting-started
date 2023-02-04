b2bf662075f75785a1b7c01ab619b48480e0730c
homegrowncheese <shanis9er@me.com>
2023-02-03 19:41:23 -0800

    add forums

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,10 +1,10 @@
+import hello.views
 from django.urls import path, include
 
-from django.contrib import admin
+from django.contrib import admin, include
 
 admin.autodiscover()
 
-import hello.views
 
 # To add a new path, first import the app:
 # import blog
@@ -18,4 +18,5 @@ urlpatterns = [
     path("", hello.views.index, name="index"),
     path("db/", hello.views.db, name="db"),
     path("admin/", admin.site.urls),
+    path('forums/', include('forums.urls')),
 ]
